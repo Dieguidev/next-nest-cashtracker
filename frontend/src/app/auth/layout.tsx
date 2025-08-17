f0bd2a57410452affeb11d5765ca2be1b8b1b1ad
Dieguidev <diegogaraycullas@gmail.com>
2025-08-17 13:49:22 -0500

    feat(ui): improve layout and homepage design
    - Updated `auth/layout.tsx` to include a clickable logo linking to the homepage.
    - Redesigned `page.tsx` to provide a detailed description of the platform, including benefits and navigation links for login and registration.
    - Enhanced user experience with improved visual hierarchy

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -1,25 +1,24 @@
 import { Logo } from "@/components";
+import Link from "next/link";
 
 export default function RootAuthLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <>
       <div className="lg:grid lg:grid-cols-2 lg:min-h-screen">
-
         <div className="bg-purple-950 flex justify-center bg-auth">
-
           <div className="w-96 py-10 lg:py-20">
-            <Logo />
+            <Link href="/">
+              <Logo />
+            </Link>
           </div>
         </div>
 
         <div className="p-10 lg:py-28">
-          <div className="max-w-3xl mx-auto">
-            {children}
-          </div>
+          <div className="max-w-3xl mx-auto">{children}</div>
         </div>
       </div>
     </>
